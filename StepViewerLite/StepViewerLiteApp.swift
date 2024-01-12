198dcea4a7b27d8d1ac21aa4a8f1adf3ede4a11c
Emily Schell <lauraemilyschell@gmail.com>
2024-01-12 14:11:26 -0500

    Initial Commit

---
+++ StepViewerLite/StepViewerLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepViewerLiteApp.swift
+//  StepViewerLite
+//
+//  Created by Emily Schell on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct StepViewerLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
